9f4d961582f7d965b33e7deb8b9cbbea0de70f76
judevalens <judevalens@gmail.com>
2021-05-09 21:34:12 -0400

    save

--- gui/src/index.js
+++ gui/src/index.js
@@ -1,64 +1,8 @@
-const {
-  app,
-  BrowserWindow
-} = require('electron');
-const path = require('path');
-const {ipcMain} = require('electron')
-
-
-const DawnTorrentIPC = require('./ipc.js')
-const dawnTorrentIPC = new DawnTorrentIPC()
-dawnTorrentIPC.addToQueue()
-
-//dawnTorrentIPC.getUpdates()
-
-
-const createWindow = () => {
-  // Create the browser window.
-  const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      nodeIntegration: true
-    }
-  });
-
-  // and load the index.html of the app.
-  mainWindow.loadFile(path.join(__dirname, 'statics/html/index.html'));
-
-  // Open the DevTools.
- // mainWindow.webContents.openDevTools();
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
 };
-ipcMain.on("sendToDaswnTorrentClient", (event , arg) => {
-  console.log(arg) // prints "ping"
-  event.reply =  event.reply('sendToDawnTorrentClientR', 'pong')
-})
-
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
-
-// Quit when all windows are closed.
-app.on('window-all-closed', () => {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
-});
-
-
-
-app.on('activate', () => {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow();
-  }
-});
-
-
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and import them here.
+Object.defineProperty(exports, "__esModule", { value: true });
+var App_1 = __importDefault(require("./App"));
+var main = new App_1.default();
+main.setUp();
